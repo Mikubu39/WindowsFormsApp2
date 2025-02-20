2f0227f3b841b8b6f39a799c9596b2f1a97fadc0
Mikubu39 <Nicek92408@gmail.com>
2025-02-19 17:34:12 -0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+     
+ 
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textAge = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textClass = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cMCuni2DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.cMCuni2DataSet = new WindowsFormsApp2.CMCuni2DataSet();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cMCuni2DataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cMCuni2DataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(131, 89);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(100, 22);
+            this.textName.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(275, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "age";
+            // 
+            // textAge
+            // 
+            this.textAge.Location = new System.Drawing.Point(325, 91);
+            this.textAge.Name = "textAge";
+            this.textAge.Size = new System.Drawing.Size(100, 22);
+            this.textAge.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(460, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Class";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(675, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textClass
+            // 
+            this.textClass.Location = new System.Drawing.Point(507, 92);
+            this.textClass.Name = "textClass";
+            this.textClass.Size = new System.Drawing.Size(100, 22);
+            this.textClass.TabIndex = 6;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.DataSource = this.cMCuni2DataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(51, 173);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(585, 248);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // cMCuni2DataSetBindingSource
+            // 
+            this.cMCuni2DataSetBindingSource.DataSource = this.cMCuni2DataSet;
+            this.cMCuni2DataSetBindingSource.Position = 0;
+            // 
+            // cMCuni2DataSet
+            // 
+            this.cMCuni2DataSet.DataSetName = "CMCuni2DataSet";
+            this.cMCuni2DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textClass);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textAge);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cMCuni2DataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cMCuni2DataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textAge;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.TextBox textClass;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource cMCuni2DataSetBindingSource;
+        private CMCuni2DataSet cMCuni2DataSet;
+    }
+}
+
---
+++ StudentContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+using System.Data.Entity;
+namespace WindowsFormsApp2
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext() : base("name=StudentDBConnectionString")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
